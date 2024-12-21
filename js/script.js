9a046ddc4133de7f344db8780ef692f3809b2ee1
Worgzz333r <golubeckola@gmail.com>
2024-12-21 13:00:53 +0200

    Final maybe

--- js/script.js
+++ js/script.js
@@ -29,7 +29,8 @@ let products_list = document.querySelector(".products-list")
 
 function getCard(product) {
     return `
-    <div class="product-panel mb-5">
+    <div class="product-shadow mb-5">
+    <div class="product-panel">
         <div class="stone-image m-2">
             <img class="product-img" src="img/${product.image}" alt="">
         </div>
@@ -46,6 +47,7 @@ function getCard(product) {
             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 17h-11v-14h-2" /><path d="M6 5l14 1l-1 7h-13" /></svg>
             </button>
         </div>
+    </div>
     </div>`
 }
 
@@ -149,7 +151,7 @@ if (cart_list) {
 
     clear_btn?.addEventListener("click", function () {
         document.cookie = `cart=''; max-age=0; path/`
-        // cart_list.innerHTML = 'Your cart is empty'
+        cart_list.innerHTML = 'Your cart is empty'
         clear_cart_btn.classList.add('d-none')
         go_to_order_btn.classList.add('d-none')
     })
